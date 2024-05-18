d577d2e4c8e145de5ba1a15b95a4aa45c7e15c2d
diegovalencia0 <129540391+diegovalencia0@users.noreply.github.com>
2024-05-17 23:53:15 -0600

    Add files via upload

---
+++ desarolloWeb/tareasIndividuales/ntmlCheatsSheet/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3002;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'html_cheat_sheet.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}/`);
+});
